004028daae98467cebe21807c8d58f6c3f9f7be9
Paris Leoni Sharples <98209682+parissharples@users.noreply.github.com>
2023-04-17 13:00:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import React, { useState } from 'react';
-import './App.css';
-
+import React, { useState } from &#39;react&#39;;
+import &#39;./App.css&#39;;
+import &#39;./paris style.css&#39;
 const answers = [
-  "It is certain",
-  "Without a doubt",
-  "You may rely on it",
-  "Yes definitely",
-  "It is decidedly so",
-  "As I see it, yes",
-  "Most likely",
-  "Yes",
-  "Outlook good",
-  "Signs point to yes",
-  "Reply hazy try again",
-  "Better not tell you now",
-  "Ask again later",
-  "Cannot predict now",
-  "Concentrate and ask again",
-  "Don't count on it",
-  "Outlook not so good",
-  "My sources say no",
-  "Very doubtful",
-  "My reply is no"
+&quot;It is certain&quot;,
+&quot;Without a doubt&quot;,
+&quot;You may rely on it&quot;,
+&quot;Yes definitely&quot;,
+&quot;It is decidedly so&quot;,
+&quot;As I see it, yes&quot;,
+&quot;Most likely&quot;,
+&quot;Yes&quot;,
+&quot;Outlook good&quot;,
+&quot;Signs point to yes&quot;,
+&quot;Reply hazy try again&quot;,
+&quot;Better not tell you now&quot;,
+&quot;Ask again later&quot;,
+&quot;Cannot predict now&quot;,
+&quot;Concentrate and ask again&quot;,
+&quot;Don&#39;t count on it&quot;,
+&quot;Outlook not so good&quot;,
+&quot;My sources say no&quot;,
+&quot;Very doubtful&quot;,
+&quot;My reply is no&quot;
 ];
-
 function App() {
-  const [answer, setAnswer] = useState("");
-
-  const shakeBall = () => {
-    const randomIndex = Math.floor(Math.random() * answers.length);
-    setAnswer(answers[randomIndex]);
-  };
-
-  return (
-    <div className="App">
-      <h1>Magic 8 Ball</h1>
-      <button onClick={shakeBall}>Shake the ball!</button>
-      {answer && (
-        <div className="answer">
-          <p>{answer}</p>
-        </div>
-      )}
-    </div>
-  );
+const [answer, setAnswer] = useState(&quot;Shake the ball!&quot;);
+const shakeBall = () =&gt; {
+const randomIndex = Math.floor(Math.random() * answers.length);
+setAnswer(answers[randomIndex]);
+};
+return (
+&lt;div className=&quot;App&quot;&gt;
+&lt;h1&gt;Magic 8 Ball&lt;/h1&gt;
+&lt;button onClick={shakeBall}&gt;
+{answer &amp;&amp; (
+&lt;div className=&quot;answer&quot;&gt;
+{answer}
+&lt;/div&gt;
+)}
+&lt;/button&gt;
+&lt;/div&gt;
+);
 }
-
 export default App;
